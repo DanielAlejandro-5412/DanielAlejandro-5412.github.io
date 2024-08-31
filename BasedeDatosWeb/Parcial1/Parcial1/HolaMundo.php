b57748a65e35790f855053e21e003a1a810e461e
DanielAlejandro-5412 <159593483+DanielAlejandro-5412@users.noreply.github.com>
2024-08-30 18:43:28 -0700

    HolaMundoPhP 30/08/2024

---
+++ BasedeDatosWeb/Parcial1/Parcial1/HolaMundo.php
@@ -0,0 +1,19 @@
+<?php 
+
+$nombre = "Daniel";
+$numero = 4;
+$numero2 = "4";
+
+$total = $numero + $numero2;
+
+echo "Total =" . $total;
+
+echo "<h1> HOLA MUNDO </h1>";
+
+for($i=0; $i<11; $i++){
+    echo $i . "<br>";
+}
+
+echo "<h3 style='color: blueviolet;'> López Martínez Daniel Alejandro </h3>";
+
+?>
